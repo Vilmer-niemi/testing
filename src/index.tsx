0b8d73d7678a0972e5e1977823a3c5daaf93c789
Vilmer Niemi <75931083+Vilmer-niemi@users.noreply.github.com>
2025-05-22 12:20:10 +0200

    updated index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import './index.css'; // Assuming you have a basic CSS file for Tailwind directives
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Assuming you have a basic CSS file for Tailwind directives
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
